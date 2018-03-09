a0af4fcf5fa3a3b9b32f29bd1a29bdea2c579b12
Kenneth Leo Hansen <abenhansen@gmail.com>
2018-03-09 11:09:05 +0100

    Add files via upload

--- src/main/java/com/example/demo/Employee.java
+++ src/main/java/com/example/demo/Employee.java
@@ -1,4 +1,101 @@
 package com.example.demo;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Employee {
+    private int ID;
+    private String fornavn;
+    private String efternavn;
+    private int alder;
+    private String stilling;
+    private int telefonnr;
+
+    public Employee(int ID, String fornavn, String efternavn, int alder, String stilling, int telefonnr) {
+        this.ID = ID;
+        this.fornavn = fornavn;
+        this.efternavn = efternavn;
+        this.alder = alder;
+        this.stilling = stilling;
+        this.telefonnr = telefonnr;
+    }
+
+    Employee(){
+
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getFornavn() {
+        return fornavn;
+    }
+
+    public void setFornavn(String fornavn) {
+        this.fornavn = fornavn;
+    }
+
+    public String getEfternavn() {
+        return efternavn;
+    }
+
+    public void setEfternavn(String efternavn) {
+        this.efternavn = efternavn;
+    }
+
+    public int getAlder() {
+        return alder;
+    }
+
+    public void setAlder(int alder) {
+        this.alder = alder;
+    }
+
+    public String getStilling() {
+        return stilling;
+    }
+
+    public void setStilling(String stilling) {
+        this.stilling = stilling;
+    }
+
+    public int getTelefonnr() {
+        return telefonnr;
+    }
+
+    public void setTelefonnr(int telefonnr) {
+        this.telefonnr = telefonnr;
+    }
+
+    public void GemEmpFil(String file) throws Exception  {
+        PrintStream outputToFile = new PrintStream(new FileOutputStream(file, true));
+        String employee = String.format("%d %s %s %d %s %d",
+                ID, fornavn, efternavn, alder, stilling,telefonnr);
+        outputToFile.println(employee);
+        outputToFile.flush();
+    }
+
+    public static void GemEmp(String file,ArrayList<Employee> employees) throws Exception {
+        new PrintStream(new File(file));
+        for (int i = 0; i < employees.size(); i++) {
+            employees.get(i).GemEmpFil(file);
+        }
+    }
+    public static void hentEmp(String file, ArrayList<Employee> employees) throws Exception {
+        Scanner scanner = new Scanner(new File(file));
+
+        while (scanner.hasNextLine()) {
+            employees.add(new Employee(scanner.nextInt(), scanner.next(), scanner.next(), scanner.nextInt(), scanner.next(), scanner.nextInt()));
+            scanner.nextLine();
+        }
+        scanner.close();
+    }
 }
--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -8,21 +8,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
-import java.util.Date;
 
 @Controller
 public class HomeController {
     boolean load = true;
     ArrayList<Kid> barn = new ArrayList<>();
+    ArrayList<Employee> employees = new ArrayList<Employee>();
 
     @GetMapping("/")
     public String index (){
         return "index";
     }
 
+    @GetMapping("index")
+    public String indexredirect (){
+        return "index";
+    }
 
-    @GetMapping("oplys")
-    public String oplys(Model model) throws Exception {
+
+    @GetMapping("oplysKid")
+    public String oplysKid(Model model) throws Exception {
        /* if (barn.size() == 0) {
             barn.add(new Kid(1, "Svend", "Erik", "2", "Lars Larsen", "2121121", new Date()));
         }*/
@@ -31,57 +36,123 @@ public class HomeController {
             load=false;
         }
         model.addAttribute("barn", barn);
-        return "oplys";
+        return "oplysKid";
     }
     /*
 
      */
 
-    @GetMapping("/create")
-    public String create (Model model) {
+    @GetMapping("/createKid")
+    public String createKid (Model model) {
         model.addAttribute("kid", new Kid());
-        return "create";
+        return "createKid";
     }
 
-    @PostMapping("/create")
-    public String create(@ModelAttribute Kid kid) throws Exception{
+    @PostMapping("/createKid")
+    public String createKid(@ModelAttribute Kid kid) throws Exception{
         int index = barn.size() + 1;
         kid.setID(index);
         barn.add(kid);
         Kid.GemKid("C:\\Users\\abenh\\Desktop\\GardenKinder\\src\\main\\resources\\static\\GemteKids.txt", barn);
-        return "redirect:/oplys";
+        return "redirect:/oplysKid";
     }
 
-    @GetMapping("/details")
-    public String details(@RequestParam(value = "id", defaultValue = "1") int id, Model model) {
+    @GetMapping("/detailsKid")
+    public String detailsKid(@RequestParam(value = "id", defaultValue = "1") int id, Model model) {
         model.addAttribute("kid", barn.get(id - 1));
-        return "details";
+        return "detailsKid";
     }
 
-    @GetMapping("/delete")
-    public String delete(@RequestParam(value = "id", defaultValue = "1") int id, Model model) {
+    @GetMapping("/deleteKid")
+    public String deleteKid(@RequestParam(value = "id", defaultValue = "1") int id, Model model) {
         model.addAttribute("kid", barn.get(id - 1));
-        return "delete";
+        return "deleteKid";
     }
 
-    @PostMapping("/delete")
-    public String delete(@ModelAttribute Kid kid) {
+    @PostMapping("/deleteKid")
+    public String deleteKid(@ModelAttribute Kid kid) throws Exception {
         barn.remove(kid.getID() - 1);
-        return "redirect:/oplys";
+        Kid.GemKid("C:\\Users\\abenh\\Desktop\\GardenKinder\\src\\main\\resources\\static\\GemteKids.txt", barn);
+        return "redirect:/oplysKid";
     }
 
-    @GetMapping("/edit")
-    public String edit(@RequestParam(value = "id", defaultValue = "1") int id, Model model) {
+    @GetMapping("/editKid")
+    public String editKid(@RequestParam(value = "id", defaultValue = "1") int id, Model model) {
         model.addAttribute("kid", barn.get(id - 1));
-        return "edit";
+        return "editKid";
     }
 
-    @PostMapping("/edit")
-    public String edit(@ModelAttribute Kid kid) {
+    @PostMapping("/editKid")
+    public String editKid(@ModelAttribute Kid kid) throws Exception {
         int index = kid.getID();
         kid.setID(index);
         barn.set(index -1,kid);
-        return "redirect:/oplys";
+        Kid.GemKid("C:\\Users\\abenh\\Desktop\\GardenKinder\\src\\main\\resources\\static\\GemteKids.txt", barn);
+        return "redirect:/oplysKid";
+    }
+
+    @GetMapping("oplysEmp")
+    public String oplysEmp(Model model) throws Exception {
+       /* if (barn.size() == 0) {
+            barn.add(new Kid(1, "Svend", "Erik", "2", "Lars Larsen", "2121121", new Date()));
+        }*/
+        if (load) {
+            Employee.hentEmp("C:\\Users\\abenh\\Desktop\\GardenKinder\\src\\main\\resources\\static\\GemteEmp.txt", employees);
+            load=false;
+        }
+        model.addAttribute("employees", employees);
+        return "oplysEmp";
+    }
+    /*
+
+     */
+
+    @GetMapping("/createEmp")
+    public String createEmp (Model model) {
+        model.addAttribute("employee", new Employee());
+        return "createEmp";
+    }
+
+    @PostMapping("/createEmp")
+    public String createEmp(@ModelAttribute Employee employee) throws Exception{
+        int index = employees.size() + 1;
+        employee.setID(index);
+        employees.add(employee);
+        Employee.GemEmp("C:\\Users\\abenh\\Desktop\\GardenKinder\\src\\main\\resources\\static\\GemteEmp.txt", employees);
+        return "redirect:/oplysEmp";
+    }
+
+    @GetMapping("/detailsEmp")
+    public String detailsEmp(@RequestParam(value = "id", defaultValue = "1") int id, Model model) {
+        model.addAttribute("employee", employees.get(id - 1));
+        return "detailsEmp";
+    }
+
+    @GetMapping("/deleteEmp")
+    public String deleteEmp(@RequestParam(value = "id", defaultValue = "1") int id, Model model) {
+        model.addAttribute("employee", employees.get(id - 1));
+        return "deleteEmp";
+    }
+
+    @PostMapping("/deleteEmp")
+    public String deleteEmp(@ModelAttribute Employee employee) throws Exception {
+        employees.remove(employee.getID() - 1);
+        return "redirect:/oplysEmp";
+    }
+
+    @GetMapping("/editEmp")
+    public String editEmp(@RequestParam(value = "id", defaultValue = "1") int id, Model model) {
+        model.addAttribute("employee", employees.get(id - 1));
+        return "editEmp";
+    }
+
+    @PostMapping("/editEmp")
+    public String editEmp(@ModelAttribute Employee employee) throws Exception {
+        int index = employee.getID();
+        employee.setID(index);
+        employees.set(index -1,employee);
+        Employee.GemEmp("C:\\Users\\abenh\\Desktop\\GardenKinder\\src\\main\\resources\\static\\GemteEmp.txt", employees);
+        return "redirect:/oplysEmp";
     }
 }
 
